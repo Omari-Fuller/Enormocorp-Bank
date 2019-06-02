8f3230850b8555181785606924eb7cb334048722
Omari-Fuller <43542826+Omari-Fuller@users.noreply.github.com>
2019-06-02 13:53:33 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,31 @@
+var deposit;
+var name;
+var totalDeposit;
+var depositNames=[];
+var depositAmounts=[];
+
+$(".newDeposit").click(function() {
+    
+    totalDeposit=0;
+    deposit = $(".accountDeposit").val();
+    name = $(".accountName").val();
+    $(".ledger").append("<p>" + name + "</p><p class='ledgerBorder'>$" + deposit + "</p>");
+     
+     depositAmounts.push(parseInt(deposit));
+     depositNames.push(name);
+    
+
+    depositAmounts.forEach(function(total) {
+        totalDeposit = totalDeposit + parseInt(total);
+        $(".number").text("Number of Transactions: " + depositAmounts.length);
+        $(".total").text("Total: $" + totalDeposit);
+    });
+
+
+$(".update").click(function(){
+    console.log(depositAmounts[depositAmounts.length]);
+   alert("Your last depsoit amount was $"+depositAmounts[depositAmounts.length-1]+" by "+depositNames[depositNames.length-1]);
+ 
+});
+
+});
